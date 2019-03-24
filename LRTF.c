3b61c37bec4f70bc00e83894fa719294225e61b5
bin780 <joytom780@gmail.com>
2019-03-25 00:00:40 +0530

    first commit

---
+++ LRTF.c
@@ -0,0 +1,13 @@
+#include<unistd.h>
+#include<stdio.h>
+
+struct student     // structure for details of each student
+{
+	int student_idl;
+	int arrivalTime;
+	int FoodTime;
+};
+int main()
+{
+	
+}
