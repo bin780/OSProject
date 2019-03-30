f4eb0d735bcdbef3d2e5cb7ced4389f129de94a1
bin780 <joytom780@gmail.com>
2019-03-30 21:49:37 +0530

    third commit

--- LRTF.c
+++ LRTF.c
@@ -6,12 +6,31 @@ struct student     // structure for details of each student
 	int student_id;
 	int arrivalTime;
 	int FoodTime;
+	int completionTime;
+	int turnAroundTime;
+	int waitingTime;
+	int backUp;
 };
-struct student s[50];               //global variable
+int findBiggest(int);
+void sort();
+void display();
+void calculate();
+void findCompletionTime();
+
+
+
+struct student s[50];                //global variables
+
+int totalTime=0;                                   
+int time=0;
+int totalWT;
+int totalTAT;
+int n=3;                           //total no of student by default 3
+
 void input()
 {
 	                                           
-	int n,i;
+	int i;
 	printf("\nenter the number of students\n");
 	scanf("%d",n);   
 	for(i=0;i<n;++i)                                                             //function to take custom input
@@ -25,19 +44,128 @@ void input()
 	}
 }
 struct student init()
-{
+{   printf("first here");
     //struct student s;
-    int i;
+    int i,j;
     s[0].student_id=2132;  
     s[1].student_id=2102;                                                      //function to initialise the values
     s[2].student_id=2453;
-	for(i=2;i<=8;i*2)
+	for(i=2,j=0;i<=8;i=i*2,j++)
 	{
-		s[i].arrivalTime=0;
-		s[i].FoodTime=i;
-	}	
+		s[j].arrivalTime=0;
+		s[j].FoodTime=i;
+		s[j].backUp=s[j].FoodTime;
+		time+=s[j].FoodTime;
+	}
+	
 } 
 int main()
 {
 	
+	init();
+	sort();
+	totalTime+=s[0].arrivalTime;
+	
+	time+=s[0].arrivalTime;
+	findCompletionTime();
+	calculate();
+	display();
+	
+	
+	
+	
+}
+void display()
+{
+	int i;
+	for(i=0;i<n;++i)
+	{
+		printf("\nstudent_id %d",s[i].student_id);
+		printf("\nstudent foodtime %d",s[i].backUp);
+		printf("\nstudent arrival time %d",s[i].arrivalTime);
+		printf("\nstudent completion time %d",s[i].completionTime);
+		printf("\nstudent waiting time %d",s[i].waitingTime);
+		printf("\nstudnt turn around time %d\n\n",s[i].turnAroundTime);
+	}
+	printf("\naverage total TAT %f",totalTAT/(float)n);
+    printf("\naverage waiting time %f",totalWT/(float)n);
+	
+}
+
+void calculate()
+{
+	totalWT=0;
+	totalTAT =0;
+	int i;
+	for(i=0;i<4;i++)
+	{
+	       s[i].turnAroundTime=s[i].completionTime-s[i].arrivalTime;
+	       s[i].waitingTime=s[i].turnAroundTime-s[i].backUp;
+		totalWT+=s[i].waitingTime;
+		totalTAT+=s[i].turnAroundTime;
+	}
+}
+void sort()
+{  int i,j;
+  struct student temp;
+    //printf("loo started");
+    
+	for(i=0;i<n-1;++i)
+	{
+		for(j=0;j<n-i-1;j++)
+		{   //printf("hi");
+			if(s[j].arrivalTime>s[j+1].arrivalTime)
+			{
+				temp=s[j+1];
+				s[j+1]=s[j];
+				s[j]=temp;
+				
+			}
+		}
+	}
+	//printf("loop finished");
+}
+
+
+
+void findCompletionTime()
+{  
+
+    //printf("here");
+	int index;
+	int flag=0;
+	int i =s[0].arrivalTime;
+	
+	while(1)
+	{
+		index = findBiggest(i);
+		
+	     printf("student taking food at time %d is : student_id %d\n",totalTime,s[index].student_id);
+	     s[index].FoodTime-=1;
+	     totalTime+=1;
+	     i++;
+	     
+	     if(s[index].FoodTime==0)
+	     {
+	     	s[index].completionTime=totalTime;
+	     	printf("\nstudent %d has completed taking his food at time %d\n",s[index].student_id,totalTime);
+	     	
+		 }
+		 if(totalTime == time)
+		   break;
+	     
+	}
+}
+
+int findBiggest(int t)
+{  int max=0,i;
+	for(i=0;i<n;i++)
+	{
+		if(s[i].arrivalTime<=t)
+		{
+			if(s[i].FoodTime>s[max].FoodTime)
+			    max=i;
+		}
+	}
+	return max;
 }
